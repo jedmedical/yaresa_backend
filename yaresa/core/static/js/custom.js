4230e95eeeca358ce340e070f6c576fd01be056f
Sammie473 <38797211+Sammie473@users.noreply.github.com>
2018-10-01 03:33:19 +0100

    the personal info form

--- yaresa/core/static/js/custom.js
+++ yaresa/core/static/js/custom.js
@@ -1,2 +1,12 @@
 // Animations init
      new WOW().init();
+	 
+	 $('#others').material_chip({
+    placeholder:'Add Disease.',
+    secondaryPlaceholder: 'Add Disease And Press Enter.',
+});
+
+$('#currentmed').material_chip({
+    placeholder:'Add Medication.',
+    secondaryPlaceholder:'Add Medication.',
+});
