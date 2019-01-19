f91002176ed6f3841d91aa8e723cbae5dccb0cd0
Andrews Agyemang Opoku <andrewsopoku6@gmail.com>
2019-01-19 23:40:14 +0000

    Add doctor view added

--- yaresa/core/views/core_view.py
+++ yaresa/core/views/core_view.py
@@ -559,8 +559,82 @@ def user_lipidprofile(request,pk):
     return render(request, 'user-lipid-profile.html', context)
 
 
+def add_doctor(request):
+
+    if request.method == "POST":
+        new_user_form = NewUserForm(request.POST, request.FILES)
+
+        if new_user_form.is_valid():
+            picture = new_user_form.cleaned_data['picture']
+            title = new_user_form.cleaned_data['title']
+            first_name = new_user_form.cleaned_data['first_name']
+            other_name = new_user_form.cleaned_data['other_name']
+
+            surname = new_user_form.cleaned_data['surname']
+            sex = new_user_form.cleaned_data['sex']
+
+            date_of_birth = new_user_form.cleaned_data['date_of_birth']
+
+            nationality = new_user_form.cleaned_data['nationality']
+            religion = new_user_form.cleaned_data['religion']
+            marital_status = new_user_form.cleaned_data['marital_status']
+            address = new_user_form.cleaned_data['address']
+
+            email = new_user_form.cleaned_data['email']
+            mobile = new_user_form.cleaned_data['mobile']
+            # blood_group = new_user_form.cleaned_data['blood_group']
+            # sickling_status = new_user_form.cleaned_data['sickling_status']
+            # g6pd = new_user_form.cleaned_data['g6pd']
+
+            pin = random.randint(1000,9999)
+
+            now = datetime.datetime.now()
+
+            try:
+                user = User.objects.create_user(username=email, password=pin,
+                                               )
+                user.is_staff = True
+                user.groups.add(Group.objects.get_or_create(name="Doctor")[0])
+
+                user_info = AuthUserDemographic(user=user,email=email,picture=picture,
+                                                title=title,first_name=first_name,other_name=other_name,
+                                                surname=surname,sex=sex,date_of_birth=date_of_birth,
+                                                nationality=nationality,religion=religion,
+                                                marital_status=marital_status,address=address,
+                                                mobile=mobile
+
+                                                )
 
 
+                user_info.save()
+
+                user_info.unique_id = '{}{}{}{}{}'.format('D',now.day,now.month,
+                                                        now.year,add_zeros(5,str(user_info.id)))
+
+                user_info.save()
+
+                sendsms(request,mobile,pin)
+
+
+
+                messages.success(request, "Doctor added")
+            except IntegrityError as e:
+                # if 'unique constraint' in e.args[0]:
+                    messages.error(request, 'User already exist')
+
+        else:
+            print("Andrews")
+
+
+
+
+
+        context = {'new_user_form':new_user_form}
+        return render(request,'add_doctor.html',context)
+
+    new_user_form = NewUserForm()
+    context = {'new_user_form':new_user_form}
+    return render(request,'add_doctor.html',context)
 
 
 
