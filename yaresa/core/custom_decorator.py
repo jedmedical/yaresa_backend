3703f94d04e97c9f3f28973da3340936b62e99ab
Andrews Agyemang Opoku <andrewsopoku6@gmail.com>
2019-01-27 22:54:20 +0000

    Has_group template tag added

---
+++ yaresa/core/custom_decorator.py
@@ -0,0 +1,17 @@
+__author__ = 'Andy'
+
+from django.contrib.auth.decorators import user_passes_test
+
+
+def user_in_groups(group_names):
+    def is_in_group(user):
+        return _is_user_belong_to_group(user, group_names)
+    return user_passes_test(is_in_group, "/status/401")
+
+
+def _is_user_belong_to_group(user, group_names):
+    return bool(user.groups.filter(name__in=group_names))
+
+
+def is_a_jed_doc():
+    return user_in_groups(["Doctor"])
